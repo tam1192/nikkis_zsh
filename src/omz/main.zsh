142643ac52b705d754b256a5063d5e3ed3bae02b
tam1192 <tamkame123@outlook.com>
2026-07-28 00:12:45 +0900

    omzモジュールのzsh_custom変数を削除

--- src/omz/main.zsh
+++ src/omz/main.zsh
@@ -65,7 +65,7 @@ export ZSH="$SHELL_CONFIG/omz.d"
 
 # Would you like to use another custom folder than $ZSH/custom?
 # ZSH_CUSTOM=/path/to/new-custom-folder
-ZSH_CUSTOM="$HOME/.zsh_custom"
+# ZSH_CUSTOM="$HOME/.zsh_custom"
 
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
