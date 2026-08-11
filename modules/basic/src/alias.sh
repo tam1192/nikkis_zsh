a5e38829bd8b1fe279cffa7cab67e9b9901be6b7
tam1192 <tamkame123@outlook.com>
2026-08-12 01:36:25 +0900

    feat: basicを新バージョンに対応

---
+++ modules/basic/src/alias.sh
@@ -0,0 +1,16 @@
+# 1語
+alias a='alias'
+alias c='cat'
+alias e='echo'
+alias t='touch'
+
+# 3語
+alias clr='clear'           #ターミナルバッファクリア
+alias rst='reset'           #ターミナルリセット
+alias vrc="v ~/.zshrc"      #zshrc 編集
+alias src="source ~/.zshrc" #zshrc 適用
+
+# ネットワーク系
+alias ns='nslookup'
+alias p='ping -c 4'
+alias pp='ping'
