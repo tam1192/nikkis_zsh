b30a56fb83a63072e4e796d38fd0ff6e2d713064
tam1192 <tamkame123@outlook.com>
2026-08-17 01:34:00 +0900

    refact(omzt-robbyrussell): 新規格に変更

--- modules/omzt-robbyrussell/main.mk
+++ modules/omzt-robbyrussell/main.mk
@@ -1,11 +1,14 @@
-$(BUILD_DIR)/omzt-robbyrussell.main: $(BUILD_DIR)/omzt-robbyrussell/main.zsh
-	@cp $^ $@
+# ==============================================================================
+# Module: OmztRobbyrussell
+# ==============================================================================
 
-$(BUILD_DIR)/omzt-robbyrussell.alias:
-	@touch $@
+MOD_OMZT_ROBBYRUSSEL        := omzt-robbyrussell
+MOD_OMZT_ROBBYRUSSEL_DIR    := $(MODULES_DIR)/$(MOD_OMZT_ROBBYRUSSEL)
 
-$(BUILD_DIR)/omzt-robbyrussell.path:
-	@touch $@
+ifneq ($(SHELL_TYPE), zsh)
+	# zsh のみ対応
+	$(error [FATAL ERROR] MODULES 'omzt-robbyrussell' はSHELL_TYPE 'zsh' 以外対応していません。ビルドを強制終了します。)
+endif
 
 $(BUILD_DIR)/omzt-robbyrussell.omzp:
 	@touch $@
@@ -18,5 +21,22 @@ ifeq ($(filter omz,$(MODULES)),)
     $(error [FATAL ERROR] omzモジュールが必要です。 ビルドを強制終了します。)
 endif
 
-$(OUT_DIR)/$(CONFIG_DIR)/omzt-robbyrussell.d:
-	@mkdir -p $@
+# ------------------------------------------------------------------------------
+# Build Rules
+# ------------------------------------------------------------------------------
+
+# 存在しない必須ファイルを生成
+$(MOD_OMZT_ROBBYRUSSEL_DIR)/main.sh:
+	@> $@
+
+$(MOD_OMZT_ROBBYRUSSEL_DIR)/main.env:
+	@> $@
+
+# ------------------------------------------------------------------------------
+# Phonies & Clean
+# ------------------------------------------------------------------------------
+.PHONY: omzt-robbyrussell-clean
+
+# 必須: クリーン処理 (存在しないファイルがあってもエラーにならないよう -f を付与)
+omzt-robbyrussell-clean:
+	@rm -f $(MOD_OMZT_ROBBYRUSSEL_DIR)/main.sh $(MOD_OMZT_ROBBYRUSSEL_DIR)/main.env
